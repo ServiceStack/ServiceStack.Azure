15a8b6c8c72c16839b3fa005a276446030f928a3
Sergey Zhukov <svg@ngs.ru>
2017-07-06 15:31:02 +0700

    Add build scripts

---
+++ build/build.bat
@@ -0,0 +1,5 @@
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+
+%MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
+%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
+REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;PatchVersion=9
