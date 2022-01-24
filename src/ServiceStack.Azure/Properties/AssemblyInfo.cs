814707604de14d43f589973e7073cd77ae9c731e
Demis Bellot <demis.bellot@gmail.com>
2022-01-24 22:16:45 +0800

    Upgrade to v6

--- src/ServiceStack.Azure/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Azure/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿[assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("eb41d580-f1da-4fef-a3c7-9be53238821e")]
-[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("6.0.0.0")]
